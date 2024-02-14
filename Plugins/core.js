a3d364d1703fa6635097c9be716763c362f7e0af
botskaking <152233761+botskaking@users.noreply.github.com>
2024-02-14 08:18:05 +0530

    Update core.js

--- Plugins/core.js
+++ Plugins/core.js
@@ -44,7 +44,7 @@ module.exports = {
           2
         )} MB\n*📅 Last Updated:* ${repo.updated_at}\n\n*🔗 Repo Link:* ${
           repo.html_url
-        }\n\n❝ Dont forget to give a Star ⭐ to the repo. It's made with restless hardwork by *Team ATLAS*. ❞\n\n*©️ Team ATLAS- 2023*`;
+        }\n\n❝ Dont forget to give a Star ⭐ to the repo. It's made with restless hardwork by *Team IRONBOTZ*. ❞\n\n*©️ Team IRONBOTZ- 2024*`;
         Atlas.sendMessage(m.from, { image: pic, caption: txt }, { quoted: m });
         break;
 
